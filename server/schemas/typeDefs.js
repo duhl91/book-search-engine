b54810ca6ab3759f006400a714cb6570eea7c8ae
dalton uhl <dalton.uhl@yahoo.com>
2021-11-15 01:48:03 -0500

    heroku deploy

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+// require the apollo-server-exrpress
+const { gql } = require("apollo-server-express");
+
+// typeDefs object
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+    bookId: String!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    users: [User]
+    getSingleUser(user: String!): User
+    me: User
+  }
+  input SaveBook {
+    authors: [String]
+    title: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(book: SaveBook): User
+    removeBook(bookid: String!): User
+  }
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-const express = require("express");
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("./schemas");
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
-const path = require("path");
-const db = require("./config/connection");
-const routes = require("./routes");
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
 app.use(express.urlencoded({ extended: true }));
@@ -25,7 +26,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
